4e0ad207aec3a895a6fbb6101918f79227a56fc4
Skeith Yip <skeith.yip@triquesta.com>
2022-09-04 16:04:15 +0800

    fix: update reducer test

--- src/reducers/__tests__/reduceStudentGrade.test.js
+++ src/reducers/__tests__/reduceStudentGrade.test.js
@@ -15,16 +15,15 @@ describe('Reducer should reduce data to student key with courses and gpa', () =>
   });
 
   test('should has student gpa', () => {
-    expect(reduce(sample).grades[1902001]?.gpa).toBe(2);
+    expect(reduce(sample).grades[1902001]?.gpa).toBe('2.0');
   });
 
   test('should has student ids', () => {
     expect(reduce(sample).ids).toEqual([
-      '1902004',
-      '1902003',
       '1902001',
       '1902002',
       '1902003',
+      '1902004',
     ]);
   });
 });
